c28548d8a212682e261af785411425fa7cf01b2c
Teffer31 <t.barmak7@gmail.com>
2023-09-28 13:37:26 +0200

    idk

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    Transform target;
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float shootInterval = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(Shoot());
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,4 +34,9 @@ public class Tower : MonoBehaviour
         Vector2 direction = target.position - transform.position;
         transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
     }
+
+    //IEnumerator Shoot()
+    //{
+//
+    //}
 }
