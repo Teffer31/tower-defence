4dc303076bc88199f1765ce124d41d47fe0a7344
Teffer31 <t.barmak7@gmail.com>
2023-11-01 06:58:45 +0100

    Update EnemySpawner.cs

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -4,59 +4,25 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [System.Serializable]
-    public class Round
+    [SerializeField] private List<GameObject> enemyPrefabs = new List<GameObject>();
+    [SerializeField] private float spawnInterval = 1.0f;
+
+    void Start()
     {
-        public List<GameObject> enemiesToSpawn;
-        public float timeBetweenSpawns = 1.0f;
+        StartCoroutine(SpawnEnemies());
     }
 
-    [SerializeField] private List<Round> rounds = new List<Round>();
-    private int currentRound = 0;
-    private int currentEnemyIndex = 0;
-    private bool roundInProgress = false;
-
-    private void Start()
+    IEnumerator SpawnEnemies()
     {
-        StartCoroutine(ManageRounds());
-    }
-
-    IEnumerator ManageRounds()
-    {
-        while (currentRound < rounds.Count)
+        while (true)
         {
-            if (!roundInProgress)
+            yield return new WaitForSeconds(spawnInterval);
+            if (enemyPrefabs.Count > 0)
             {
-                StartCoroutine(StartRound());
+                int randomIndex = Random.Range(0, enemyPrefabs.Count);
+                GameObject randomEnemy = enemyPrefabs[randomIndex];
+                Instantiate(randomEnemy, transform.position, Quaternion.identity);
             }
-            yield return null;
         }
-    }
-
-    IEnumerator StartRound()
-    {
-        roundInProgress = true;
-        Round round = rounds[currentRound];
-        List<GameObject> enemies = round.enemiesToSpawn;
-        float spawnDelay = round.timeBetweenSpawns;
-
-        yield return new WaitForSeconds(3.0f); // Delay before starting the round.
-
-        while (currentEnemyIndex < enemies.Count)
-        {
-            Instantiate(enemies[currentEnemyIndex], transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(spawnDelay);
-            currentEnemyIndex++;
-        }
-
-        // Wait until all enemies in the round are defeated.
-        while (currentEnemyIndex > 0)
-        {
-            yield return null;
-        }
-
-        currentRound++;
-        currentEnemyIndex = 0;
-        roundInProgress = false;
     }
 }
